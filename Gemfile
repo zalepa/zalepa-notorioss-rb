e6f58f8b27f9b2fb16ae38ea86b2ad6a5fc0e66c
George Zalepa <george.zalepa@gmail.com>
2025-03-31 10:52:59 -0400

    move to tabluar display

--- Gemfile
+++ Gemfile
@@ -5,8 +5,9 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in notorioss.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
-
 gem "minitest", "~> 5.16"
-
+gem "rake", "~> 13.0"
 gem "rubocop", "~> 1.21"
+gem "terminal-table", "~> 4.0"
+
+gem "rainbow", "~> 3.1"
--- lib/notorioss.rb
+++ lib/notorioss.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "notorioss/version"
+require "terminal-table"
+require "rainbow"
 
 module Notorioss
   class Error < StandardError; end
@@ -19,6 +21,26 @@ module Notorioss
         licenses[license] << "#{spec.name} (#{spec.version})"
       end
 
+      # summarize(licenses)
+      tableize(licenses)
+    end
+
+    def self.tableize(licenses)
+      table = Terminal::Table.new headings: %w[License Count Licenses]
+
+      licenses.keys.each do |license|
+        license_name = license
+        # TEST: highlight GPL licenses
+        # Note: this is just a sample, not the final output.
+        license_name = Rainbow(license_name).bg(:red) if license.match(/GPL/)
+        table.add_row [license_name, licenses[license].count, licenses[license].sort.join("\n")]
+        table.add_separator
+      end
+
+      puts table
+    end
+
+    def self.summarize(licenses)
       puts "\nLicense Summary:"
       puts "----------------"
 
